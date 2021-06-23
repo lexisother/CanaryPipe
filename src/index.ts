e1d3893481fbe58ec7edb376073a16e47bcbdc34
Alyxia Sother <lexisoth2005@gmail.com>
2021-06-23 15:37:31 +0200

    [Main] Added basic message pipe

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("test");
+import {Client, TextChannel} from "discord.js";
+import config from "./config";
+
+const client = new Client({user: true});
+
+client.on("ready", () => {
+    console.log(`logged in as ${client.user?.username}#${client.user?.discriminator}`);
+});
+
+client.on("message", async (msg) => {
+    const sendChannel = (await client.channels.fetch("843471565986594816")) as TextChannel;
+    if (msg.channel.id === "755013928647524372" && msg.embeds.length > 0) {
+        if (msg.embeds[0].title?.includes("Experiment Added")) {
+            sendChannel!.send({embed: msg.embeds[0]});
+        }
+    }
+});
+
+client.login(config.token);
